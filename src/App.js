768c6d93cf72837e50b407120b1e1dd92886d72a
octalad <ladeiraoctavio@gmail.com>
2024-08-27 13:05:08 -0300

    footer finalizado

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Navbar from "./layout/navbar/Navbar";
+import Footer from "./layout/footer/Footer";
 
 function App() {
   return (
     <Routes>
-      <Route element={<Navbar />}>
+      <Route element={<Footer />}>
         <Route path="/" element={<Home />} />
       </Route>
     </Routes>
